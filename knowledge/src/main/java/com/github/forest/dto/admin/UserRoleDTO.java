e97e3e681664ea55abd1a110d45a35391f4d40ee
sunzhengyu99 <sunzy9901@qq.com>
2023-06-04 10:45:02 +0800

    dev: AdminController 开发

---
+++ knowledge/src/main/java/com/github/forest/dto/admin/UserRoleDTO.java
@@ -0,0 +1,15 @@
+package com.github.forest.dto.admin;
+
+import lombok.Data;
+
+/**
+ * @author sunzy
+ * @date 2023/6/4 10:34
+ */
+@Data
+public class UserRoleDTO {
+
+    private Long idUser;
+
+    private Long idRole;
+}
--- knowledge/src/main/java/com/github/forest/web/api/admin/AdminController.java
+++ knowledge/src/main/java/com/github/forest/web/api/admin/AdminController.java
@@ -7,6 +7,7 @@ import com.github.forest.core.result.GlobalResult;
 import com.github.forest.core.result.GlobalResultGenerator;
 import com.github.forest.dto.UserInfoDTO;
 import com.github.forest.dto.UserSearchDTO;
+import com.github.forest.dto.admin.UserRoleDTO;
 import com.github.forest.entity.Role;
 import com.github.forest.entity.Tag;
 import com.github.forest.entity.Topic;
@@ -52,6 +53,12 @@ public class AdminController {
         return adminService.getUsers(page, rows, searchDTO);
     }
 
+    @GetMapping("/users/{idUser}/role")
+    public GlobalResult<List<Role>> userRole(@PathVariable Long idUser) {
+        List<Role> roles = roleService.findByIdUser(idUser);
+        return GlobalResultGenerator.genSuccessResult(roles);
+    }
+
     @GetMapping("/roles")
     public GlobalResult<PageInfo<Role>> roles(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "10") Integer rows) {
         PageHelper.startPage(page, rows);
@@ -59,6 +66,25 @@ public class AdminController {
         PageInfo<Role> pageInfo = new PageInfo<>(roles);
         return GlobalResultGenerator.genSuccessResult(pageInfo);
     }
+
+    @PostMapping("/user/update-role")
+    public GlobalResult<Boolean> updateUserRole(@RequestBody UserRoleDTO userRole) throws ServiceException {
+        boolean flag = userService.updateUserRole(userRole.getIdUser(), userRole.getIdRole());
+        return GlobalResultGenerator.genSuccessResult(flag);
+    }
+
+    @PostMapping("/user/update-status")
+    public GlobalResult<Boolean> updateUserStatus(@RequestBody User user) throws ServiceException {
+        boolean flag = userService.updateStatus(user.getId(), user.getStatus());
+        return GlobalResultGenerator.genSuccessResult(flag);
+    }
+
+    @PostMapping("/role/update-status")
+    public GlobalResult<Boolean> updateRoleStatus(@RequestBody Role role) throws ServiceException{
+        boolean flag = roleService.updateStatus(role.getIdRole(), role.getStatus());
+        return GlobalResultGenerator.genSuccessResult(flag);
+    }
+
 
     @PostMapping("/role/post")
     public GlobalResult<Boolean> addRole(@RequestBody Role role) throws ServiceException {
