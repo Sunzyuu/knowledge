7d6751dce9768559bc1d21c7c3d5b7295cb42cdf
sunzhengyu99 <sunzy9901@qq.com>
2023-06-12 14:32:19 +0800

    dev: FollowService

--- knowledge/src/main/java/com/github/forest/service/FollowService.java
+++ knowledge/src/main/java/com/github/forest/service/FollowService.java
@@ -1,7 +1,10 @@
 package com.github.forest.service;
 
+import com.github.forest.dto.UserDTO;
 import com.github.forest.entity.Follow;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +15,57 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-06-12
  */
 public interface FollowService extends IService<Follow> {
+    /**
+     * 判断是否关注
+     *
+     * @param followingId
+     * @param followingType
+     * @param idUser
+     * @return
+     */
+    Boolean isFollow(Integer followingId, String followingType, Long idUser);
 
+    /**
+     * 关注操作
+     *
+     * @param follow
+     * @param nickname
+     * @return
+     */
+    Boolean follow(Follow follow, String nickname);
+
+
+    /**
+     * 取消关注操作
+     *
+     * @param follow
+     * @return
+     */
+    Boolean cancelFollow(Follow follow);
+
+    /**
+     * 获取关注用户者数据
+     *
+     * @param followType
+     * @param followingId
+     * @return
+     */
+    List<Follow> findByFollowingId(String followType, Long followingId);
+
+
+    /**
+     * 查询用户粉丝
+     *
+     * @param userDTO
+     * @return
+     */
+    List<UserDTO> findUserFollowersByUser(UserDTO userDTO);
+
+    /**
+     * 查询用户关注用户
+     *
+     * @param userDTO
+     * @return
+     */
+    List<UserDTO> findUserFollowingsByUser(UserDTO userDTO);
 }
--- knowledge/src/main/java/com/github/forest/service/impl/FollowServiceImpl.java
+++ knowledge/src/main/java/com/github/forest/service/impl/FollowServiceImpl.java
@@ -1,10 +1,16 @@
 package com.github.forest.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.github.forest.dto.UserDTO;
 import com.github.forest.entity.Follow;
 import com.github.forest.mapper.FollowMapper;
 import com.github.forest.service.FollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +22,52 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements FollowService {
+
+    @Resource
+    private FollowMapper followMapper;
+
+
+    @Override
+    public Boolean isFollow(Integer followingId, String followingType, Long idUser) {
+        return followMapper.isFollow(followingId, idUser, followingType);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public Boolean follow(Follow follow, String nickname) {
+        boolean result = save(follow);
+        if (result) {
+            // todo :: 关注消息提醒
+        }
+        return result;
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public Boolean cancelFollow(Follow follow) {
+        LambdaQueryWrapper<Follow> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(true, Follow::getFollowerId, follow.getFollowerId());
+        queryWrapper.eq(true, Follow::getFollowingId, follow.getFollowingId());
+        queryWrapper.eq(true, Follow::getFollowingType, follow.getFollowingType());
+        return remove(queryWrapper);
+    }
+
+    @Override
+    public List<Follow> findByFollowingId(String followType, Long followingId) {
+        LambdaQueryWrapper<Follow> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(true, Follow::getFollowingId, followingId);
+        queryWrapper.eq(true, Follow::getFollowingType, followType);
+        return list(queryWrapper);
+    }
+
+    @Override
+    public List<UserDTO> findUserFollowersByUser(UserDTO userDTO) {
+        return followMapper.selectUserFollowersByUser(userDTO.getIdUser());
+    }
+
+    @Override
+    public List<UserDTO> findUserFollowingsByUser(UserDTO userDTO) {
+        return followMapper.selectUserFollowingsByUser(userDTO.getIdUser());
+    }
 
 }
