915ce5bc8323b5f8ef8c64dd9702b8210ddb558c
sunzhengyu99 <sunzy9901@qq.com>
2023-06-12 15:13:29 +0800

    dev: CommentMapper

--- knowledge/src/main/java/com/github/forest/mapper/CommentMapper.java
+++ knowledge/src/main/java/com/github/forest/mapper/CommentMapper.java
@@ -1,7 +1,12 @@
 package com.github.forest.mapper;
 
+import com.github.forest.dto.Author;
+import com.github.forest.dto.CommentDTO;
 import com.github.forest.entity.Comment;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +18,42 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface CommentMapper extends BaseMapper<Comment> {
 
+    /**
+     * 获取文章的所有评论
+     * @param idArticle
+     * @return
+     */
+    List<CommentDTO> selectArticleComments(@Param("idArticle") Integer idArticle);
+
+    /**
+     * 查询评论作者
+     *
+     * @param commentAuthorId
+     * @return
+     */
+    Author selectAuthor(@Param("commentAuthorId") Integer commentAuthorId);
+
+    /**
+     * 查询父评论作者
+     *
+     * @param commentOriginalCommentId
+     * @return
+     */
+    Author selectCommentOriginalAuthor(@Param("commentOriginalCommentId") Integer commentOriginalCommentId);
+
+    /**
+     * 更新文章评论分享链接
+     *
+     * @param idComment
+     * @param commentSharpUrl
+     * @return
+     */
+    Integer updateCommentSharpUrl(@Param("idComment") Long idComment, @Param("commentSharpUrl") String commentSharpUrl);
+
+    /**
+     * 获取评论列表数据
+     *
+     * @return
+     */
+    List<CommentDTO> selectComments();
 }
