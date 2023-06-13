8c96b59269bd044b26a032f7c8248784e4e087c6
sunzhengyu99 <sunzy9901@qq.com>
2023-06-13 14:28:30 +0800

    dev: LuceneServiceImpl.searchArticle

--- knowledge/src/main/java/com/github/forest/lucene/service/impl/LuceneServiceImpl.java
+++ knowledge/src/main/java/com/github/forest/lucene/service/impl/LuceneServiceImpl.java
@@ -5,19 +5,36 @@ import com.github.forest.dto.ArticleTagDTO;
 import com.github.forest.dto.Author;
 import com.github.forest.entity.User;
 import com.github.forest.lucene.lucene.ArticleBeanIndex;
+import com.github.forest.lucene.lucene.IKAnalyzer;
 import com.github.forest.lucene.mapper.ArticleLuceneMapper;
 import com.github.forest.lucene.model.ArticleLucene;
 import com.github.forest.lucene.service.LuceneService;
 import com.github.forest.lucene.util.ArticleIndexUtil;
 import com.github.forest.lucene.util.LucenePath;
+import com.github.forest.lucene.util.SearchUtil;
 import com.github.forest.mapper.ArticleMapper;
 import com.github.forest.service.UserService;
 import com.github.forest.util.Html2TextUtil;
 import com.github.forest.util.Utils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.highlight.*;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -99,7 +116,75 @@ public class LuceneServiceImpl implements LuceneService {
 
     @Override
     public List<ArticleLucene> searchArticle(String value) {
-        return null;
+        List<ArticleLucene> resList = new ArrayList<>();
+        ExecutorService service = Executors.newCachedThreadPool();
+        // 定义分词器
+        Analyzer analyzer = new IKAnalyzer();
+        try {
+            IndexSearcher searcher =
+                    SearchUtil.getIndexSearcherByParentPath(LucenePath.ARTICLE_INDEX_PATH, service);
+            String[] fields = {"title", "summary"};
+
+            MultiFieldQueryParser parser = new MultiFieldQueryParser(fields, analyzer);
+
+            BufferedReader in =
+                    new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+            String line = value != null ? value : in.readLine();
+            Query query = parser.parse(line);
+            SimpleHTMLFormatter htmlFormatter =
+                    new SimpleHTMLFormatter("<font color=" + "\"" + "red" + "\"" + ">", "</font>");
+            // 高亮搜索的词添加到高亮处理器中
+            Highlighter highlighter = new Highlighter(htmlFormatter, new QueryScorer(query));
+            // 获取搜索的结果，指定返回document返回的个数
+            // TODO 默认搜索结果为显示第一页，1000 条，可以优化
+            TopDocs results = SearchUtil.getScoreDocsByPerPage(1, 100, searcher, query);
+            ScoreDoc[] hits = results.scoreDocs;
+            // 遍历，输出
+            for (ScoreDoc hit : hits) {
+                int id = hit.doc;
+                float score = hit.score;
+                Document hitDoc = searcher.doc(hit.doc);
+                // 获取到summary
+                String summary = hitDoc.get("summary");
+                // 将查询的词和搜索词匹配，匹配到添加前缀和后缀
+                TokenStream tokenStream = TokenSources.getTokenStream("summary", searcher.getIndexReader().getTermVectors(id), summary, analyzer, -1);
+                // 传入的第二个参数是查询的值
+                TextFragment[] frag = highlighter.getBestTextFragments(tokenStream, summary, false, 10);
+                StringBuilder baikeValue = new StringBuilder();
+                for (TextFragment textFragment : frag) {
+                    if ((textFragment != null) && (textFragment.getScore() > 0)) {
+                        //  if ((frag[j] != null)) {
+                        // 获取 summary 的值
+                        baikeValue.append(textFragment);
+                    }
+                }
+
+                // 获取到title
+                String title = hitDoc.get("title");
+                TokenStream titleTokenStream = TokenSources.getTokenStream("title", searcher.getIndexReader().getTermVectors(id), title, analyzer, -1);
+                TextFragment[] titleFrag =
+                        highlighter.getBestTextFragments(titleTokenStream, title, false, 10);
+                StringBuilder titleValue = new StringBuilder();
+                for (int j = 0; j < titleFrag.length; j++) {
+                    if ((frag[j] != null)) {
+                        titleValue.append(titleFrag[j].toString());
+                    }
+                }
+                resList.add(
+                        ArticleLucene.builder()
+                                .idArticle(Long.valueOf(hitDoc.get("id")))
+                                .articleTitle(titleValue.toString())
+                                .articleContent(baikeValue.toString())
+                                .score(String.valueOf(score))
+                                .build());
+            }
+
+        } catch (IOException | ParseException | InvalidTokenOffsetsException e) {
+            e.printStackTrace();
+        } finally {
+            service.shutdown();
+        }
+        return resList;
     }
 
     @Override
