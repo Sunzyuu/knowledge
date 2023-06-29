fb10f6aa943cadbbabff6001c42c284821129e99
sunzhengyu99 <sunzy9901@qq.com>
2023-06-29 11:24:19 +0800

    dev: 为openai服务添加代理，解决连接超时问题

--- knowledge/src/main/java/com/github/forest/openai/OpenAiController.java
+++ knowledge/src/main/java/com/github/forest/openai/OpenAiController.java
@@ -1,24 +1,32 @@
 package com.github.forest.openai;
 
 import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.forest.core.result.GlobalResult;
 import com.github.forest.core.result.GlobalResultGenerator;
 import com.github.forest.entity.User;
 import com.github.forest.openai.service.OpenAiService;
 import com.github.forest.openai.service.SseService;
 import com.github.forest.util.UserUtils;
+import com.theokanning.openai.OpenAiApi;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatMessage;
+import okhttp3.OkHttpClient;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import retrofit2.Retrofit;
 
 import javax.annotation.Resource;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.time.Duration;
 import java.util.ArrayList;
+
+import static com.github.forest.openai.service.OpenAiService.*;
 
 /**
  * @author sunzy
@@ -36,6 +44,14 @@ public class OpenAiController {
 
     @PostMapping("/chat")
     public GlobalResult chat(@RequestBody JSONObject jsonObject) {
+        ObjectMapper mapper = defaultObjectMapper();
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 7890));
+        OkHttpClient client =  defaultClient(token,Duration.ofSeconds(10000))
+                .newBuilder()
+                .proxy(proxy)
+                .build();
+        Retrofit retrofit = defaultRetrofit(client, mapper);
+        OpenAiApi api = retrofit.create(OpenAiApi.class);
         String message = jsonObject.getString("message");
         if (StringUtils.isBlank(message)) {
             throw new IllegalArgumentException("参数异常！");
@@ -44,7 +60,7 @@ public class OpenAiController {
         ChatMessage chatMessage = new ChatMessage("user", message);
         ArrayList<ChatMessage> chatMessages = new ArrayList<>();
         chatMessages.add(chatMessage);
-        OpenAiService service = new OpenAiService(token, Duration.ofSeconds(180));
+        OpenAiService service = new OpenAiService(api);
         ChatCompletionRequest completionRequest = ChatCompletionRequest.builder()
                 .model("gpt-3.5-turbo")
                 .stream(true)
