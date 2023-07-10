4dce70d420dbf3c5d117d8e157475bef2ed1764e
sunzhengyu99 <sunzy9901@qq.com>
2023-07-10 18:25:40 +0800

    dev: BankAccountService

--- knowledge/src/main/java/com/github/forest/service/BankAccountService.java
+++ knowledge/src/main/java/com/github/forest/service/BankAccountService.java
@@ -1,7 +1,12 @@
 package com.github.forest.service;
 
+import com.github.forest.dto.BankAccountDTO;
+import com.github.forest.dto.BankAccountSearchDTO;
+import com.github.forest.dto.TransactionRecordDTO;
 import com.github.forest.entity.BankAccount;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +17,60 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-06-29
  */
 public interface BankAccountService extends IService<BankAccount> {
+    /**
+     * 查询银行账户列表
+     *
+     * @param bankAccountSearchDTO
+     * @return
+     */
+    List<BankAccountDTO> findBankAccounts(BankAccountSearchDTO bankAccountSearchDTO);
 
+    /**
+     * 查询用户银行账户
+     *
+     * @param idUser
+     * @return
+     */
+    BankAccountDTO findBankAccountByIdUser(Long idUser);
+
+    /**
+     * 根据账户查询银行账户信息
+     *
+     * @param bankAccount
+     * @return
+     */
+    BankAccountDTO findByBankAccount(String bankAccount);
+
+    /**
+     * 查询系统社区银行
+     *
+     * @return
+     */
+    BankAccount findSystemBankAccount();
+
+    /**
+     * 查询账号信息
+     *
+     * @param formBankAccount
+     * @return
+     */
+    BankAccount findInfoByBankAccount(String formBankAccount);
+
+    /**
+     * 根据时间查询账号交易记录
+     *
+     * @param bankAccount
+     * @param startDate
+     * @param endDate
+     * @return
+     */
+    List<TransactionRecordDTO> findUserTransactionRecords(String bankAccount, String startDate, String endDate);
+
+    /**
+     * 创建钱包账号
+     *
+     * @param idUser
+     * @return
+     */
+    BankAccount createBankAccount(Long idUser);
 }
--- knowledge/src/main/java/com/github/forest/service/impl/BankAccountServiceImpl.java
+++ knowledge/src/main/java/com/github/forest/service/impl/BankAccountServiceImpl.java
@@ -1,10 +1,16 @@
 package com.github.forest.service.impl;
 
+import com.github.forest.dto.BankAccountDTO;
+import com.github.forest.dto.BankAccountSearchDTO;
+import com.github.forest.dto.TransactionRecordDTO;
 import com.github.forest.entity.BankAccount;
 import com.github.forest.mapper.BankAccountMapper;
 import com.github.forest.service.BankAccountService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +23,43 @@ import org.springframework.stereotype.Service;
 @Service
 public class BankAccountServiceImpl extends ServiceImpl<BankAccountMapper, BankAccount> implements BankAccountService {
 
+
+    @Resource
+    private BankAccountMapper bankAccountMapper;
+
+
+    @Override
+    public List<BankAccountDTO> findBankAccounts(BankAccountSearchDTO bankAccountSearchDTO) {
+        return null;
+    }
+
+    @Override
+    public BankAccountDTO findBankAccountByIdUser(Long idUser) {
+        return null;
+    }
+
+    @Override
+    public BankAccountDTO findByBankAccount(String bankAccount) {
+        return null;
+    }
+
+    @Override
+    public BankAccount findSystemBankAccount() {
+        return null;
+    }
+
+    @Override
+    public BankAccount findInfoByBankAccount(String formBankAccount) {
+        return null;
+    }
+
+    @Override
+    public List<TransactionRecordDTO> findUserTransactionRecords(String bankAccount, String startDate, String endDate) {
+        return null;
+    }
+
+    @Override
+    public BankAccount createBankAccount(Long idUser) {
+        return null;
+    }
 }
